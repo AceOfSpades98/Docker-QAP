f81022b5675fff7f201078ac90a4729cb42bddca
Noah Whiffen <Noah.Whiffen@keyin.com>
2025-07-24 11:51:24 -0230

    repository classes created

--- src/main/java/tournaments/TournamentRepository.java
+++ src/main/java/tournaments/TournamentRepository.java
@@ -1,4 +1,9 @@
 package tournaments;
 
-public class TournamentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TournamentRepository extends JpaRepository<Tournament, Long> {
+
 }
