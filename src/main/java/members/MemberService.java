a73c4e7cc6941687d6bf260db6372876261fa62c
Noah Whiffen <Noah.Whiffen@keyin.com>
2025-07-24 12:05:45 -0230

    Comments added to service classes to show methods that need to be added

--- src/main/java/members/MemberService.java
+++ src/main/java/members/MemberService.java
@@ -1,4 +1,26 @@
 package members;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MemberService {
+    @Autowired
+    MemberRepository memberRepository;
+
+    // Search by name
+
+    // Search by membership type
+
+    // Search by phone number
+
+    // Search by start date of tournament
+
+    // Get all members
+
+    // Create member
+
+    // Update member
+
+    // Delete member
 }
--- src/main/java/tournaments/TournamentService.java
+++ src/main/java/tournaments/TournamentService.java
@@ -1,4 +1,24 @@
 package tournaments;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TournamentService {
+    @Autowired
+    public TournamentRepository tournamentRepository;
+
+    // Search by start date
+
+    // Search by location
+
+    // Find all members in a tournament
+
+    // Get all tournaments
+
+    // Create a tournament
+
+    // Update a tournament
+
+    // Delete a tournament
 }
