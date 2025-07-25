334fce9c1a2c1cbea128ad853180d742e850568f
Noah Whiffen <Noah.Whiffen@keyin.com>
2025-07-25 14:34:54 -0230

    implemented most methods in memberService and changed all instances of dates from Strings to LocalDate types.

--- src/main/java/tournaments/TournamentRepository.java
+++ src/main/java/tournaments/TournamentRepository.java
@@ -3,7 +3,10 @@ package tournaments;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TournamentRepository extends JpaRepository<Tournament, Long> {
 
+    List<Tournament> findByStartDate(String startDate);
 }
