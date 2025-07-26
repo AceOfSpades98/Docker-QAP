a86f13a4308b8f12f36949dca2c5081cfc2214fb
Noah Whiffen <Noah.Whiffen@keyin.com>
2025-07-26 21:01:57 -0230

    constructors and variables added to both controllers, and comments stating what each method needs to be

--- src/main/java/app/members/MemberController.java
+++ src/main/java/app/members/MemberController.java
@@ -1,10 +1,42 @@
 package app.members;
 
+import app.tournaments.TournamentController;
+import app.tournaments.TournamentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 @RequestMapping
 public class MemberController {
+
+    private final MemberService memberService;
+    private final TournamentService tournamentService;
+
+    @Autowired
+    public MemberController(MemberService memberService, TournamentService tournamentService) {
+        this.memberService = memberService;
+        this.tournamentService = tournamentService;
+    }
+
+    // Search by ID
+
+    // Search by name
+
+    // Search by membership type
+
+    // Search by email
+
+    // Search by phone number
+
+    // Search by start date of a tournament
+
+    // Get all members
+
+    // Create member
+
+    // Update member
+
+    // Delete member
 
 }
--- src/main/java/app/tournaments/TournamentController.java
+++ src/main/java/app/tournaments/TournamentController.java
@@ -1,4 +1,42 @@
 package app.tournaments;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping
 public class TournamentController {
+
+    private final TournamentService tournamentService;
+
+    @Autowired
+    public TournamentController(TournamentService tournamentService) {
+        this.tournamentService = tournamentService;
+    }
+
+    // Search by start date
+
+    // Search by location
+
+    // Find all members in a tournament
+
+    // Add members to a tournament
+
+    // Get all tournaments
+    @GetMapping
+    public List<Tournament> getAllTournaments() {
+        return tournamentService.getAllTournaments();
+    }
+
+    // Create a tournament
+
+    // Update a tournament
+
+    // Delete a tournament
 }
